e246d7f1d87bbd0c3c358556cc941b1be519bb33
chris-shay <christopher.jackson.shay@gmail.com>
2017-02-28 19:24:40 -0500

    Learn function composition

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,35 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+    (if (= severity :mild)
+      "MILDLY INCONVENIENCED!"
+      "DOOOOOOMMEDDD!")))
+; (error-message :mild)
+(defn too-enthusiastic
+  "Return a cheer that might be a bit too enthusiatic"
+  [name]
+  (str "OH. MY. GOD! " name " YOU ARE DEFINITELY LIKE THE BEST "
+  "MAN SLASH WOMAN EVER I LOVE YOU AND WE SHOULD RUN AWAY SOMEWHERE"))
+
+  (too-enthusiastic "Zelda")
+  ; 
+  ; (defn multi-arity
+  ;   ; 3-arity arguments and body
+  ;   ([first-arg second-arg third-arg]
+  ;     (do-things first-arg second-arg third-arg))
+  ;     ; 2-arity arguments and body
+  ;     ([first-arg second-arg]
+  ;       (do-things first-arg second-arg))
+  ;       ; 1-arity arguments and body
+  ;     ([first-arg]
+  ;       (do-things first-arg)))
+
+(defn x-chop
+  "Describe the kind of chop you're inflicting on someone"
+  ([name chop-type]
+    (str "I " chop-type " chop " name "! Take that!"))
+    ([name]
+      (x-chop name "karate")))
